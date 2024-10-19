d949f13641e96338bdce6724c5d243f234be2b6e
Daru <mica.tui@proton.me>
2024-10-19 16:52:33 +0200

    src/clock: clock works

--- src/commands/clock.rs
+++ src/commands/clock.rs
@@ -1,3 +1,10 @@
+use chrono::{prelude::*, DateTime};
+use std::{thread::sleep, time::Duration};
+
 pub fn run() {
-    println!("This should be a clock");
+    loop {
+        let local: DateTime<Local> = Local::now().try_into().unwrap();
+        print!("\r{}", local.format("%H:%M:%S").to_string());
+        sleep(Duration::from_millis(10));
+    }
 }
