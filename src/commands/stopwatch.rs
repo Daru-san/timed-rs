091e33ae778af2f5f4d2cde7fc99d5997514f1eb
Daru <mica.tui@proton.me>
2024-10-19 16:52:43 +0200

    src/stopwatch: stopwatch works

--- src/commands/stopwatch.rs
+++ src/commands/stopwatch.rs
@@ -1,3 +1,23 @@
+use std::{
+    thread::sleep,
+    time::{Duration, Instant},
+};
+
 pub fn run() {
-    println!("This should be a stopwatch\n");
+    let now = Instant::now();
+    loop {
+        let time = get_time(now.elapsed().as_millis().try_into().unwrap());
+        print!("\r{}", time);
+        sleep(Duration::from_millis(1));
+    }
+}
+fn get_time(millisec: i32) -> String {
+    let time: String;
+    let milliseconds = millisec % 100;
+    let seconds = (millisec / 1000) % 60;
+    let min = (millisec / 3600) % 60;
+    let hours = seconds / 3600;
+    time = format!("{:02}:{:02}:{:02}:{:02}", hours, min, seconds, milliseconds);
+
+    return time.to_string();
 }
