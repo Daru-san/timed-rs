8c3a322ef540268da365f26c60188c09e646988c
Daru <mica.tui@proton.me>
2024-10-19 16:54:06 +0200

    src/test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
-mod commands;
-
-use commands::{clock, stopwatch, timer};
+use std::io;
 
 use clap::Parser;
 use core::f64;
 use std::process::exit;
+
+use ratatui::{backend::CrosstermBackend, Terminal};
+
+use crate::{
+    app::{App, AppResult},
+    commands::{clock, stopwatch, timer},
+    event::{Event, EventHandler},
+    handler::handle_key_events,
+    tui::Tui,
+};
+
+pub mod app;
+pub mod commands;
+pub mod event;
+pub mod handler;
+pub mod tui;
+pub mod ui;
 
 #[derive(Parser, Debug)]
 #[clap(author = "Daru", version, about)]
@@ -23,7 +38,8 @@ struct Args {
     stopwatch: bool,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> AppResult<()> {
     let args = Args::parse();
     let enable_timer = !(args.time == 0.0);
     let enable_stopwatch = args.stopwatch;
@@ -49,4 +65,30 @@ fn main() {
     if !enable_timer && !enable_stopwatch && !enable_verbosity {
         clock::run();
     }
+    // Create an application.
+    let mut app = App::new();
+
+    // Initialize the terminal user interface.
+    let backend = CrosstermBackend::new(io::stdout());
+    let terminal = Terminal::new(backend)?;
+    let events = EventHandler::new(250);
+    let mut tui = Tui::new(terminal, events);
+    tui.init()?;
+
+    // Start the main loop.
+    while app.running {
+        // Render the user interface.
+        tui.draw(&mut app)?;
+        // Handle events.
+        match tui.events.next().await? {
+            Event::Tick => app.tick(),
+            Event::Key(key_event) => handle_key_events(key_event, &mut app)?,
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
+        }
+    }
+
+    // Exit the user interface.
+    tui.exit()?;
+    Ok(())
 }
