062aad930779bec0918cb7562a937a88f838937d
Daru <mica.tui@proton.me>
2024-10-28 22:19:10 +0200

    src/timer: attempt at timer

--- src/commands/timer.rs
+++ src/commands/timer.rs
@@ -1,3 +1,34 @@
-pub fn run(time: f64) {
-    println!("You selected time {}", time.to_string())
+use chrono::{self, NaiveTime};
+use std::{
+    thread::sleep,
+    time::{Duration, Instant},
+};
+
+pub fn run(time: f32) {
+    let now = Instant::now();
+
+    let mut time_left = (time * 1000.0) as f64;
+    let mut time_fmt;
+    loop {
+        time_left -= 1.0;
+
+        sleep(Duration::from_millis(1));
+
+        time_fmt = get_time(time_left);
+
+        print!("\r{}", time_left);
+        if time_left == 0.0 {
+            break;
+        }
+    }
+}
+
+fn get_time(millisec: f64) -> String {
+    let milliseconds = millisec.floor() as u32;
+    let seconds = (milliseconds / 1000) % 60;
+    let min = (milliseconds / 3600) % 60;
+    let hours = seconds / 3600;
+
+    let timer = NaiveTime::from_hms_milli_opt(hours, min, seconds, milliseconds);
+    timer.unwrap().to_string()
 }
