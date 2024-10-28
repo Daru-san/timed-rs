bc6932e8e28ded5c95e9a7dbc21b50bb84eedcb1
Daru <mica.tui@proton.me>
2024-10-28 21:55:46 +0200

    src/clock: take format as a parameter

--- src/commands/clock.rs
+++ src/commands/clock.rs
@@ -1,10 +1,11 @@
 use chrono::{prelude::*, DateTime};
 use std::{thread::sleep, time::Duration};
 
-pub fn run() {
+pub fn run(format: &str) {
     loop {
         let local: DateTime<Local> = Local::now().try_into().unwrap();
         print!("\r{}", local.format("%H:%M:%S").to_string());
+        print!("\r{}", local.format(format));
         sleep(Duration::from_millis(10));
     }
 }
