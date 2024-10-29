127c3865520aba6f8d93d9d3f8b6f4f7bae4fd2b
Daru <mica.tui@proton.me>
2024-10-29 22:43:24 +0200

    src/stopwatch: create struct

--- src/commands/stopwatch.rs
+++ src/commands/stopwatch.rs
@@ -1,24 +1,23 @@
-use chrono::{self, TimeDelta};
+use crate::time;
+
 use std::{
     thread::sleep,
     time::{Duration, Instant},
 };
 
-pub fn run() {
-    let now = Instant::now();
-    loop {
-        let time = get_time(now.elapsed().as_millis().try_into().unwrap());
-        print!("\r{}", time);
-        sleep(Duration::from_millis(1));
+#[derive(Debug)]
+struct Stopwatch();
+
+impl Stopwatch {
+    fn new() {
+        Stopwatch;
     }
-}
-
-fn get_time(millisec: i32) -> String {
-    let timer = TimeDelta::milliseconds(millisec.into());
-    let seconds = timer.num_seconds() % 60;
-    let min = timer.num_minutes() % 60;
-    let hours = timer.num_hours();
-    let mil = timer.num_milliseconds() % 100;
-
-    format!("{:02}:{:02}:{:02}:{:02}", hours, min, seconds, mil)
+    fn run() {
+        let now = Instant::now();
+        loop {
+            let time = time::get_time(now.elapsed().as_millis().try_into().unwrap());
+            print!("\r{}", time);
+            sleep(Duration::from_millis(1));
+        }
+    }
 }
