65d8e44a5bc4081f1f19332c01dcf1bb41ab8f02
Daru <mica.tui@proton.me>
2024-10-29 22:34:16 +0200

    src/clock: create clock struct

--- src/commands/clock.rs
+++ src/commands/clock.rs
@@ -1,10 +1,23 @@
 use chrono::{prelude::*, DateTime};
 use std::{thread::sleep, time::Duration};
 
-pub fn run(format: &str) {
-    loop {
-        let local: DateTime<Local> = Local::now();
-        print!("\r{}", local.format(format));
-        sleep(Duration::from_millis(10));
+#[derive(Debug)]
+struct Clock {
+    format: String,
+}
+
+impl Clock {
+    fn new(clock_format: String) {
+        Clock {
+            format: clock_format,
+        };
+    }
+    fn run(&self) {
+        let format = &self.format;
+        loop {
+            let local: DateTime<Local> = Local::now();
+            print!("\r{}", local.format(format));
+            sleep(Duration::from_millis(10));
+        }
     }
 }
